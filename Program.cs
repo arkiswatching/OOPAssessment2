29cd440d5a13d9dba5e13f9cb40fd02e56a2e34a
arkiswatching <61419869+arkiswatching@users.noreply.github.com>
2020-03-23 15:20:45 +0000

    edits and fixes
    finished writing file, added comments and made it run

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace OOPAssessment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First file input
+            Console.Write("Please input the first file you would wish to compare: ");
+            string selectionA = Console.ReadLine();
+            string fileA = File.ReadAllText(@"..\..\..\..\..\" + selectionA);
+            //Second file input
+            Console.Write("Please input the second file you would wish to compare: ");
+            string selectionB = Console.ReadLine();
+            string fileB = File.ReadAllText(@"..\..\..\..\..\" + selectionB);
+            //File Comparison
+            //files are not the same
+            if (fileB != fileA)
+            {
+                Console.WriteLine("The files are not the same.");
+            }
+            //Files are the same
+            else if (fileB == fileA)
+            {
+                Console.WriteLine("The files are the same.");
+            }    
+        }
+    }
+}
