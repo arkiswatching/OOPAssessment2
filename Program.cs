f97c68606fbb36d3c2a42375efab596cdfeda3ab
arkiswatching <61419869+arkiswatching@users.noreply.github.com>
2020-03-25 16:18:35 +0000

    edits made responding to a serious error
    changed the searched folder to be inside the program instead of reading outside.

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ namespace OOPAssessment2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //First file input
             Console.Write("Please input the first file you would wish to compare: ");
             string selectionA = Console.ReadLine();
-            string fileA = File.ReadAllText(@"..\..\..\..\..\" + selectionA);
+            string fileA = File.ReadAllText(selectionA);
             //Second file input
             Console.Write("Please input the second file you would wish to compare: ");
             string selectionB = Console.ReadLine();
-            string fileB = File.ReadAllText(@"..\..\..\..\..\" + selectionB);
+            string fileB = File.ReadAllText(selectionB);
             //File Comparison
             //files are not the same
             if (fileB != fileA)
@@ -25,7 +25,9 @@ namespace OOPAssessment2
             else if (fileB == fileA)
             {
                 Console.WriteLine("The files are the same.");
-            }    
+            }
+            //restart the process
+            Main();
         }
     }
 }
